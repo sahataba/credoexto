e86ee31a9426612ca340e05cc7e2add938cf15b6
Rudolf Markulin <rudolf.markulin@protonmail.com>
2020-01-03 18:34:46 +0100

    tag line Y coordinate simple calculation

--- app/src/main/java/com/example/emmanuelozibo/todolistapp/Todo/TodoItemDecoration.kt
+++ app/src/main/java/com/example/emmanuelozibo/todolistapp/Todo/TodoItemDecoration.kt
@@ -3,7 +3,7 @@ package com.example.emmanuelozibo.todolistapp.Todo
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Rect
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
@@ -14,6 +14,21 @@ class TodoItemDecoration(var spacing: Int) : RecyclerView.ItemDecoration() {
         val paint = Paint()
         paint.color = Color.BLUE
 
-        c?.drawLine(100f, 0f, 100f, 800f,  paint)
+        val lm = parent!!.layoutManager!! as LinearLayoutManager
+        val start = lm.findFirstCompletelyVisibleItemPosition()
+        val end = lm.findLastCompletelyVisibleItemPosition()
+
+        val startView: View? = parent.getChildAt(start)
+        val endView = parent.getChildAt(end)
+
+        val padd = startView?.y!!
+
+        val h: Float = ((startView?.measuredHeight)?.toFloat()) ?: 0f
+        val sY = h * (start - start) + h/2 + padd
+        val eY = h * (end - start) + h/2 + padd
+
+
+        c?.drawLine(100f, sY, 100f, eY,  paint)
+
     }
 }
