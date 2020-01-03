db4cc02816b1246f8c0406bd6984de4a1994e551
Rudolf Markulin <rudolf.markulin@protonmail.com>
2020-01-03 15:00:25 +0100

    prepopulate db

--- app/src/main/java/com/example/emmanuelozibo/todolistapp/data/local/TodoListDatabase.kt
+++ app/src/main/java/com/example/emmanuelozibo/todolistapp/data/local/TodoListDatabase.kt
@@ -1,11 +1,14 @@
 package com.example.emmanuelozibo.todolistapp.data.local
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.arch.persistence.room.TypeConverter
+import android.arch.persistence.room.migration.Migration
 import android.content.Context
 import com.example.emmanuelozibo.todolistapp.data.local.models.Todo
+import java.util.concurrent.Executors
 
 //You can also check out type converters
 @Database(entities = [Todo::class], version = 1, exportSchema = false)
@@ -28,6 +31,16 @@ abstract class TodoListDatabase: RoomDatabase(){
                 todoListDatabase = Room.databaseBuilder(context,
                         TodoListDatabase::class.java,
                         TodoListDatabase.databaseName)
+                        .addCallback(object : Callback() {
+                            override fun onCreate( db: SupportSQLiteDatabase) {
+                                super.onCreate(db);
+                                Executors.newSingleThreadScheduledExecutor().execute(object : Runnable {
+                                    override fun run() {
+                                        getInstance(context)?.getTodoDao()?.saveTodo(Todo("prvi", 1, 2, "", 1))
+                                    }
+                                })
+                            }
+                        })
                         .allowMainThreadQueries()//i will remove this later, database are not supposed to be called on main thread
                         .build()
             }
